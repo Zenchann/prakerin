03fc18667d9d28da137501bf90b2e45c2335882b
Candra <candraherdiansyah14@gmail.com>
2019-07-05 14:38:20 +0700

    ganti judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ahmad Candra
                 </div>
 
                 <div class="links">
