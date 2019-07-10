2c7e7bdad53b17d1aa0fecc736fe72c416d0f7db
Candra <candraherdiansyah14@gmail.com>
2019-07-10 08:21:29 +0700

    cors

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(
-    ['middleware' => 'cors', 'as' => 'api.'],
+    ['as' => 'api.', 'middleware' => ['cors']],
     function () {
         // Json
         Route::get('kategori', 'KategoriController@getjson')->name('json_kategori');
